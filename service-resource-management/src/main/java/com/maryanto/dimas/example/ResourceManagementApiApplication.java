8f016182e36237ae8aa633ac5c9c4fe0bbe0921c
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-06-03 22:59:27 +0700

    inital service

---
+++ service-resource-management/src/main/java/com/maryanto/dimas/example/ResourceManagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceManagementApiApplication.class, args);
+	}
+}
